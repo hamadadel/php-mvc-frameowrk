e049ca723e7854904bdacab48ed9ebc54191e78c
Hamad Adel <hamadwebdeveloper@gmail.com>
2022-06-05 23:15:32 +0200

    test and explore some test routes

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@ return function(Router $router)
     $router->add('GET', '/has-server-error', fn() => throw new Exception());
     $router->add('GET', '/has-validation-error', fn() => $router->dispatchNotAllowed());
     $router->add('GET', '/auth/login', fn() => '<h1>Login Page</h1>');
-    $router->add('GET', '/api/todos', function() {
+    $router->add('GET', '/api/todo', function() {
         $todos = [
           [
               'id'=>1,
@@ -29,5 +29,30 @@ return function(Router $router)
         return json_encode($todos, JSON_PRETTY_PRINT);
     });
 //    $router->errorHandler(404, fn() => 'whoops');
-    $router->add('GET', '/rockets', fn() => '<h3>RocketController@index</h3>');
+//    $router->add('GET', '/rockets/{id}', fn() => '<h3>RocketController@index</h3>');
+//    $router->add('GET', '/invoices/{id}', fn() => '<p>invoice number #898</p>');
+//    $router->add('GET', '/products/view/{product}/{id}', fn() => '<p>product Dell::3537</p>');
+
+    $router->add('GET', '/products/view/{product}/{id}', function () use($router) {
+        $parameters = $router->current()->parameters();
+        var_dump($parameters);
+       return "product is {$parameters['product']} with #{$parameters['id']}";
+    });
+
+    $router->add('GET', 'blog/{slug?}', function() use($router) {
+    return $router->current()->parameters()['slug'];
+    });
+
+    $router->add('GET', '/profile/{id}', function() use ($router) {
+       return "<p>Welcome to your profile number# {$router->current()->parameters()['id']}</p>";
+    });
+    $router->add(
+        'GET', '/products/{page?}',
+        function () use ($router) {
+            $parameters = $router->current()->parameters();
+            $parameters['page'] ??= 1;
+            return "products for page {$parameters['page']}";
+        },
+    );
+//    ->name('product-list');
 };
